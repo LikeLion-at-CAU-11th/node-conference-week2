a4f0c5d4b5c924114b881e3ac45f4c2d23760325
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2023-10-10 15:28:51 +0900

    feat: post 기능 추가

--- server/config/express.js
+++ server/config/express.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 module.exports = function () {
     const app = express();
     app.use(express.json());
-
+    app.use(express.urlencoded({ extended: false }));
     app.use(express.static(path.join(process.cwd(), 'views')));        // 정적 파일 제공 경로 지정 (css 적용 경로 설정)
     app.set('views', path.join(process.cwd(), 'views'));               // views 폴더 지정
     app.set('view engine', 'ejs');                                     // 템플릿 엔진 설정 
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,5 +1,43 @@
+const Todo = require("./models");
+
 module.exports = function (app) {
     const todo = require('./controller');
 
     app.get("/", todo.getTodoList);
+    
+    app.post('/add', async (req, res)=>{
+        // console.log(req.body);
+        const newTodo = new Todo({
+            todo: req.body.todo,
+            date: req.body.date,
+            rank: req.body.rank
+        });
+        console.log(newTodo);
+        try {
+            await newTodo.save();
+            res.redirect('/');
+        } catch(err) {
+            console.error(err);
+            res.status(500).send(err.message);
+        }
+    });
 };
+
+// const newTodo = new todo({
+        //     todo: req.body.todo,
+        //     date: req.body.date,
+        //     rank: req.body.rank,
+        // });
+        // console.log(newTodo);
+        // newTodo.save((err)=>{
+        //     if(err){
+        //         console.log(err);
+        //         res.state(500).send();
+        //     }
+        //     else{
+        //         res.redirect('/');
+        //     }
+        // })
+
+        // const {todo, date, rank} = req.body;
+        // const data = await todo.create({todo, date, rank});
